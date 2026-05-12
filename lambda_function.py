b70b03da123ec4f598fb390eb8868427ee64a42a
himakar365@gmail.com <himakar365@gmail.com>
2026-05-13 02:50:26 +0530

    Success

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
 from mangum import Mangum
 from upload_image_project.asgi import application #Change to wsgi if needed or if any error occurs
 
-handler = Mangum(application, api_gateway_base_path="/prod", lifespan="off")
+handler = Mangum(application, lifespan="off")
