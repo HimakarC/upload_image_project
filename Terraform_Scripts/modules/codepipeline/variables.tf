9e609642879e97035bdbd78f9d4e8bd8c738db3b
HimakarC <116370824+HimakarC@users.noreply.github.com>
2026-05-19 19:12:55 +0530

    Create variables.tf

---
+++ Terraform_Scripts/modules/codepipeline/variables.tf
@@ -0,0 +1,13 @@
+variable "pipeline_name" {}
+
+variable "bucket_name" {}
+
+variable "github_owner" {}
+
+variable "github_repo" {}
+
+variable "github_branch" {}
+
+variable "github_token" {}
+
+variable "codebuild_project_name" {}
