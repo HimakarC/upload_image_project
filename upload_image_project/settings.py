777fa2784dbcf21ac5947d81f05c41616f4d8762
himakar365@gmail.com <himakar365@gmail.com>
2026-05-19 18:57:43 +0530

    Change few

--- upload_image_project/settings.py
+++ upload_image_project/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'upload_image_project.wsgi.application'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_STORAGE_BUCKET_NAME = 'your-bucket-name'
+AWS_STORAGE_BUCKET_NAME = 'os.environ["s3_bucket"]'
 AWS_S3_REGION_NAME = 'us-east-1'
 
 AWS_QUERYSTRING_AUTH = False
