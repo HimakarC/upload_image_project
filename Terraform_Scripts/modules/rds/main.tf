9b7295d820a7dfd6eb14f6a478a88eb3232fc41f
HimakarC <116370824+HimakarC@users.noreply.github.com>
2026-05-19 19:08:17 +0530

    Create main.tf

---
+++ Terraform_Scripts/modules/rds/main.tf
@@ -0,0 +1,58 @@
+resource "aws_db_subnet_group" "subnet_group" {
+
+  name = "${var.rds_name}-subnet-group"
+
+  subnet_ids = var.subnet_ids
+}
+
+resource "aws_security_group" "rds_sg" {
+
+  name = "${var.rds_name}-sg"
+
+  vpc_id = var.vpc_id
+
+  ingress {
+
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+
+  identifier = var.rds_name
+
+  engine = "postgres"
+
+  engine_version = "16"
+
+  instance_class = "db.t3.micro"
+
+  allocated_storage = 20
+
+  username = var.db_username
+
+  password = var.db_password
+
+  publicly_accessible = true
+
+  skip_final_snapshot = true
+
+  db_subnet_group_name = aws_db_subnet_group.subnet_group.name
+
+  vpc_security_group_ids = [
+    aws_security_group.rds_sg.id
+  ]
+}
