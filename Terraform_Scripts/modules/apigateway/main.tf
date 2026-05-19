91204726567f61f631d9a5101ede39b7f70c3b67
HimakarC <116370824+HimakarC@users.noreply.github.com>
2026-05-19 19:07:08 +0530

    Create main.tf

---
+++ Terraform_Scripts/modules/apigateway/main.tf
@@ -0,0 +1,50 @@
+resource "aws_apigatewayv2_api" "api" {
+
+  name = var.api_name
+
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "integration" {
+
+  api_id = aws_apigatewayv2_api.api.id
+
+  integration_type = "AWS_PROXY"
+
+  integration_uri = var.lambda_invoke_arn
+
+  integration_method = "POST"
+
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "route" {
+
+  api_id = aws_apigatewayv2_api.api.id
+
+  route_key = "$default"
+
+  target = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "stage" {
+
+  api_id = aws_apigatewayv2_api.api.id
+
+  name = "prod"
+
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "permission" {
+
+  statement_id = "AllowAPIGatewayInvoke"
+
+  action = "lambda:InvokeFunction"
+
+  function_name = var.lambda_name
+
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
