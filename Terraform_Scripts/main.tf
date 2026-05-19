92affe907344fa87e44aa9c0c1be69343e6c47d9
HimakarC <116370824+HimakarC@users.noreply.github.com>
2026-05-19 19:04:21 +0530

    Create main.tf

---
+++ Terraform_Scripts/main.tf
@@ -0,0 +1,68 @@
+module "lambda" {
+
+  source = "./modules/lambda"
+
+  lambda_name = "${var.environment}-${var.project_name}-lambda"
+
+  lambda_zip_path = var.lambda_zip_path
+
+  lambda_handler = var.lambda_handler
+
+  lambda_runtime = var.lambda_runtime
+}
+
+module "apigateway" {
+
+  source = "./modules/apigateway"
+
+  api_name = "${var.environment}-${var.project_name}-api"
+
+  lambda_invoke_arn = module.lambda.lambda_invoke_arn
+
+  lambda_name = module.lambda.lambda_name
+}
+
+module "rds" {
+
+  source = "./modules/rds"
+
+  rds_name = "${var.environment}-${var.project_name}-db"
+
+  db_username = var.db_username
+
+  db_password = var.db_password
+
+  subnet_ids = var.subnet_ids
+
+  vpc_id = var.vpc_id
+}
+
+module "codebuild" {
+
+  source = "./modules/codebuild"
+
+  codebuild_name = "${var.environment}-${var.project_name}-build"
+
+  github_owner = var.github_owner
+
+  github_repo = var.github_repo
+}
+
+module "codepipeline" {
+
+  source = "./modules/codepipeline"
+
+  pipeline_name = "${var.environment}-${var.project_name}-pipeline"
+
+  bucket_name = "${var.environment}-${var.project_name}-bucket"
+
+  github_owner = var.github_owner
+
+  github_repo = var.github_repo
+
+  github_branch = var.github_branch
+
+  github_token = var.github_token
+
+  codebuild_project_name = module.codebuild.project_name
+}
