c362551ab99a9b00811ad246d50ac0eae3512d25
himakar365@gmail.com <himakar365@gmail.com>
2026-05-20 16:50:09 +0530

    Updated with Terraform

--- Terraform_Scripts/modules/apigateway/main.tf
+++ Terraform_Scripts/modules/apigateway/main.tf
@@ -18,7 +18,13 @@ resource "aws_apigatewayv2_integration" "integration" {
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "route" {
+resource "aws_apigatewayv2_route" "root" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "proxy" {
 
   api_id = aws_apigatewayv2_api.api.id
 
@@ -31,7 +37,7 @@ resource "aws_apigatewayv2_stage" "stage" {
 
   api_id = aws_apigatewayv2_api.api.id
 
-  name = "prod"
+  name = "$default"
 
   auto_deploy = true
 }
