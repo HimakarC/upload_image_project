f6d9d7907acb24b85a98dae1ebce37054987e1ca
himakar365@gmail.com <himakar365@gmail.com>
2026-05-20 12:07:18 +0530

    Updated with Terraform

--- upload_image_project/settings.py
+++ upload_image_project/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'upload_image_project.wsgi.application'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_STORAGE_BUCKET_NAME = os.environ["s3_bucket"]
+AWS_STORAGE_BUCKET_NAME = os.environ.get("s3_bucket", "")
 AWS_S3_REGION_NAME = 'us-east-1'
 
 AWS_QUERYSTRING_AUTH = False
