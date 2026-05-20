7272c967abafd6af50f9ecfd84bd1e1b61e1f9be
himakar365@gmail.com <himakar365@gmail.com>
2026-05-20 09:12:00 +0530

    Updated with Terraform

--- Terraform_Scripts/main.tf
+++ Terraform_Scripts/main.tf
@@ -7,30 +7,12 @@ module "network" {
   source = "./modules/network"
 }
 
-resource "aws_lambda_function" "lambda" {
-  function_name = var.function_name
-  role          = aws_iam_role.lambda_role.arn
-  handler       = var.handler
-  runtime       = var.runtime
-  timeout       = 30
+module "lambda" {
+  source = "./modules/lambda"
 
-  # Dummy ZIP only for Terraform creation
-  filename         = "${path.module}/dummy.zip"
-  source_code_hash = filebase64sha256("${path.module}/dummy.zip")
-
-  lifecycle {
-    ignore_changes = [
-      filename,
-      source_code_hash,
-      last_modified,
-    ]
-  }
-
-  environment {
-    variables = {
-      DJANGO_SETTINGS_MODULE = "upload_image_project.settings"
-    }
-  }
+  function_name = var.lambda_function_name
+  runtime        = "python3.11"
+  handler        = "lambda_function.handler"
 }
 
 module "apigateway" {
--- Terraform_Scripts/modules/lambda/main.tf
+++ Terraform_Scripts/modules/lambda/main.tf
@@ -1,42 +1,24 @@
-resource "aws_iam_role" "lambda_role" {
-
-  name = "${var.lambda_name}-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [{
-      Effect = "Allow"
-
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "basic" {
-
-  role = aws_iam_role.lambda_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 resource "aws_lambda_function" "lambda" {
+  function_name = var.function_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = var.handler
+  runtime       = var.runtime
+  timeout       = 30
 
-  function_name = var.lambda_name
+  filename         = "${path.module}/dummy.zip"
+  source_code_hash = filebase64sha256("${path.module}/dummy.zip")
 
-  role = aws_iam_role.lambda_role.arn
+  lifecycle {
+    ignore_changes = [
+      filename,
+      source_code_hash,
+      last_modified,
+    ]
+  }
 
-  runtime = var.lambda_runtime
-
-  handler = var.lambda_handler
-
-  filename = var.lambda_zip_path
-
-  source_code_hash = filebase64sha256(var.lambda_zip_path)
-
-  timeout = 30
+  environment {
+    variables = {
+      DJANGO_SETTINGS_MODULE = "upload_image_project.settings"
+    }
+  }
 }
