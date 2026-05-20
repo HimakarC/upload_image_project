09cb44319dc150df07a60c24a8514f04709a0b83
himakar365@gmail.com <himakar365@gmail.com>
2026-05-20 12:01:47 +0530

    Updated with Terraform

--- upload_image_project/settings.py
+++ upload_image_project/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'upload_image_project.wsgi.application'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_STORAGE_BUCKET_NAME = 'os.environ["s3_bucket"]'
+AWS_STORAGE_BUCKET_NAME = os.environ["s3_bucket"]
 AWS_S3_REGION_NAME = 'us-east-1'
 
 AWS_QUERYSTRING_AUTH = False
@@ -102,7 +102,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'os.environ["DB_PASSWORD"]',
+        'PASSWORD': os.environ["DB_PASSWORD"],
         'HOST': 'database-1.c05smqw0eg8p.us-east-1.rds.amazonaws.com',
         'PORT': '5432',
         'OPTIONS': {
