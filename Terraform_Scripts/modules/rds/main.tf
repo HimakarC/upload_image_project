ec981afd3e72b28b62e4d658d6f8742565f268a0
himakar365@gmail.com <himakar365@gmail.com>
2026-05-20 18:17:53 +0530

    Updated with Terraform

--- Terraform_Scripts/modules/rds/main.tf
+++ Terraform_Scripts/modules/rds/main.tf
@@ -40,6 +40,8 @@ resource "aws_db_instance" "postgres" {
 
   allocated_storage = 20
 
+  db_name = var.db_name
+  
   username = var.db_username
 
   password = var.db_password
--- Terraform_Scripts/modules/rds/variables.tf
+++ Terraform_Scripts/modules/rds/variables.tf
@@ -1,3 +1,5 @@
+variable "db_name" {}
+
 variable "db_username" {}
 
 variable "db_password" {}
