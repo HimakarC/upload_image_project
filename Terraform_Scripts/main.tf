c5c23e6730023930bbbcf5786e2636352727ccef
himakar365@gmail.com <himakar365@gmail.com>
2026-05-23 13:21:40 +0530

    Updated with Terraform

--- Terraform_Scripts/main.tf
+++ Terraform_Scripts/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_public_access_block" "django_media" {
 resource "aws_s3_bucket_policy" "public_read_policy" {
   bucket = aws_s3_bucket.django_media.id
   depends_on = [
-    aws_s3_bucket_public_access_block.public_access
+    aws_s3_bucket_public_access_block.django_media
   ]
   policy = jsonencode({
     Version = "2012-10-17"
